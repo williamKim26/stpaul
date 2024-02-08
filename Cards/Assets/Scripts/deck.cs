0fe1f36e3106f1acbb15727be62923dc4a44566a
williamKim26 <william.kim@stpaulseoul.org>
2024-02-08 15:40:49 +0900

    deck

--- Cards/Assets/Scripts/deck.cs
+++ Cards/Assets/Scripts/deck.cs
@@ -5,18 +5,24 @@ using UnityEngine;
 public class deck : MonoBehaviour
 {
     private string[] suit = {"Spades","Hearts","Diamonds","Clubs"};
-    private card[,] cards; 
+    private List<card> cards; 
     public deck()
     {
-        cards = new card[4,13];
+        cards = new List<card>();
     }
 
     void fillDeck()
     {
         for(int i = 0; i < 4; i++){
             for(int j = 1; j < 14;++j){
-                cards[i,j]= new card(suit[i],j);
+                cards.add(new card(suit[i],j));
             }
         }
+    }
+
+    card drawCard()
+    {
+        
+        card r = cards[Next(0,51)];
     }
 }
