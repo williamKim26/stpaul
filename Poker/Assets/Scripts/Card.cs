863b1d1ec2ae3285a47c5af6af1fcaad77983ac1
williamKim26 <william.kim@stpaulseoul.org>
2024-03-14 15:32:50 +0900

    .

--- Poker/Assets/Scripts/Card.cs
+++ Poker/Assets/Scripts/Card.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class Cards : MonoBehaviour
 {
+    string[] CardSuits = new string[] {"Club","Diamond","Spades","Heart"};
+    string[] CardRank = new string[] {"2","3","4","5","6","7","8","9","10","J","Q","K","A"};
     
-    void Start()
-    {
-        
-    }
-
     
-    void Update()
-    {
-        
-    }
+    
 }
